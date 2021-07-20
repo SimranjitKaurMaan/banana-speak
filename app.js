d8fa31350d671bde7a281b50e102b4372d4a8d1f
Simranjit Kaur <simranjit.kaur@udaan.com>
2021-07-20 18:29:32 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var btnTranslate = document.querySelector("#btn-translate")
+var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
+var textOutput = document.querySelector("#text-output");
+
 function clickEventHandler(){
     console.log('clicked');
     console.log(textInput.value);
+    textOutput.innerText=textInput.value;
 }
 
 btnTranslate.addEventListener("click",clickEventHandler)
+
+
 
 
